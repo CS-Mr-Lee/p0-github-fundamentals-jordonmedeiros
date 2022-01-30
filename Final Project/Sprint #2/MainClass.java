d3d32490c4eecb3a4cab16e81d825cadf2708d9e
jordonmedeiros <90928942+jordonmedeiros@users.noreply.github.com>
2022-01-29 22:00:40 -0500

    Add files via upload

---
+++ Final Project/Sprint #2/MainClass.java
@@ -0,0 +1,35 @@
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author chenw
+ */
+public class MainClass extends Application{
+    public static void main(String[] args){
+        launch(args);
+        
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        System.out.println("hi");
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("calculator.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+}
